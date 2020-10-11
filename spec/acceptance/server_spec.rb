a068d88b1ff050435049565d542546c4b5b3d827
Bryan Powell <bryan@metabahn.com>
2020-10-11 16:55:56 -0700

    Resolve a couple of code style issues

--- spec/acceptance/server_spec.rb
+++ spec/acceptance/server_spec.rb
@@ -8,7 +8,7 @@ require_relative "support/server"
 
 RSpec.describe "parsing in context of a server" do
   before do
-    @pid = Process.fork do
+    @pid = Process.fork {
       endpoint = Async::IO::Endpoint.tcp("0.0.0.0", 9000)
 
       Async do
@@ -17,7 +17,7 @@ RSpec.describe "parsing in context of a server" do
         parser = LLHttp::Parser.new(LLHttp::Delegate.new, type: :request)
         parser.parse("GET / HTTP/1.1\r\n")
       end
-    end
+    }
   end
 
   after do
@@ -25,7 +25,7 @@ RSpec.describe "parsing in context of a server" do
   end
 
   it "parses" do
-    Timeout::timeout(1) do
+    Timeout.timeout(1) do
       expect(system("curl -v http://localhost:9000")).to be(true)
     end
   end
